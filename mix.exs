014c3e89e3ea6809174c6d3353faf8dc172d05ba
Patrick Smith <pgwsmith@gmail.com>
2024-04-22 22:35:36 +1000

    Version 0.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ensemble.MixProject do
   def project do
     [
       app: :ensemble,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
