ac59090cf71168a96a241c0f3e244a92eb134c8b
Patrick Smith <pgwsmith@gmail.com>
2024-04-22 22:43:22 +1000

    Update mix.exs for publishing to Hex

--- mix.exs
+++ mix.exs
@@ -1,13 +1,23 @@
 defmodule Ensemble.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/RoyalIcing/ensemble"
+
   def project do
     [
       app: :ensemble,
       version: "0.0.1",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "DSL for WebAssembly",
+      package: package(),
+
+      # Docs
+      name: "ensemble",
+      docs: docs(),
+      source_url: @source_url,
+      homepage_url: "https://icing.space/"
     ]
   end
 
@@ -25,6 +35,26 @@ defmodule Ensemble.MixProject do
       {:floki, ">= 0.30.0", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      name: :ensemble,
+      maintainers: ["Patrick George Wyndham Smith"],
+      licenses: ["BSD-3-Clause"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      # The main page in the docs
+      main: "ensemble",
+      # logo: "orb-logo-blue-orange.svg",
+      extras: [
+        "README.md"
+      ]
     ]
   end
 end
